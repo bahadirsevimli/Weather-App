c51a7ebb153932da2a9c78588d822690432a9cf6
bahadirsevimli <bahadirsevimli@gmail.com>
2025-01-12 00:53:52 +0300

    Function used for api data
    bugs fixed.

--- index.js
+++ index.js
@@ -4,42 +4,56 @@ function createWeatherContainer (data) {
     const box = document.createElement("div");
     box.classList.add("box");
 
-    const location = document.createElement("div");
-    location.classList.add("location");
-    box.appendChild(location);
+        const location = document.createElement("div");
+        location.classList.add("location");
+        box.appendChild(location);
 
-        const city = document.createElement("div");
-        city.classList.add("city");
-        city.textContent = "Üsküdar, İstanbul";
-        location.appendChild(city);
-        
+            const city = document.createElement("div");
+            city.classList.add("city");
+            city.textContent = "Üsküdar, İstanbul";
+            location.appendChild(city);
+            
 
-        const country = document.createElement("div");
-        country.classList.add("country");
-        location.appendChild(country);
+            const country = document.createElement("div");
+            country.classList.add("country");
+            country.textContent = data["sys"]["country"];
+            location.appendChild(country);
 
-    const container = document.createElement("div");
-    container.classList.add("container");
-    box.appendChild(container);
+        const container = document.createElement("div");
+        container.classList.add("container");
+        box.appendChild(container);
 
-        const containerWeather = document.createElement("div");
-        containerWeather.classList.add("container-weather");
-        container.appendChild(containerWeather);
+            const containerWeather = document.createElement("div");
+            containerWeather.classList.add("container-weather");
+            container.appendChild(containerWeather);
 
-            const degree = document.createElement("div");
-            degree.classList.add("degree");
+                const degree = document.createElement("div");
+                degree.classList.add("degree");
+                degree.textContent = `${Math.round(data["main"]["temp"])} °C`;
+                containerWeather.appendChild(degree);
 
-            containerWeather.appendChild(degree);
+                const situation = document.createElement("div");
+                situation.classList.add("situation");
+                situation.textContent = data["weather"][0]["description"];
+                containerWeather.appendChild(situation);
 
-            const situation = document.createElement("div");
-            situation.classList.add("situation");
-            containerWeather.appendChild(situation);
+            const containerIcon = document.createElement("div");
+            containerIcon.classList.add("situation");
+            container.appendChild(containerIcon);
 
-        const containerIcon = document.createElement("div");
-        containerIcon.classList.add("situation");
-        container.appendChild(containerIcon);
-
+                const icon = document.createElement("img");
+                icon.classList.add("icon-image");
+                icon.alt = data["weather"]["description"];
+                icon.src = `/Assets/Icons/${data["weather"][0]["icon"]}.png`;
+                containerIcon.appendChild(icon);
 
     return box;
 }
+
+axios.get("https://api.openweathermap.org/data/2.5/weather?units=metric&lat=41.01&lon=29.02&appid=882853605eda9ba83449e273e5c7e4ca")
+.then((response) => {
+    const newBox = createWeatherContainer(response.data);
+    document.body.appendChild(newBox);
+});
+
 
